30fad85c5c4a2b415abb3de124a9a0ba079cb304
Ryan Tang <ryan.tang.clu@gmail.com>
2022-06-14 17:42:09 -0400

    Associations and productTotal instance method added

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,36 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require("./db");
 
-const User = require('./models/User')
+const User = require("./models/User");
+const Product = require("./models/Product");
+const Order = require("./models/Order");
+const OrderDetail = require("./models/OrderDetail");
+const Cart = require("./models/Cart");
 
 //associations could go here!
+User.hasOne(Cart);
+Cart.belongsTo(User);
+
+User.hasMany(Order);
+Order.belongsTo(User);
+
+Cart.hasOne(Order);
+Order.belongsTo(Cart);
+
+Cart.hasMany(OrderDetail);
+OrderDetail.belongsTo(Cart);
+
+OrderDetail.belongsToMany(Product, { through: "Products_Ordered" });
+Product.belongsToMany(OrderDetail, { through: "Products_Ordered" });
 
 module.exports = {
   db,
   models: {
     User,
+    Product,
+    Order,
+    OrderDetail,
+    Cart,
   },
-}
+};
--- server/db/models/OrderDetail.js
+++ server/db/models/OrderDetail.js
@@ -1,10 +1,19 @@
-const Sequelize = require('sequelize');
-const db = require('../db');
+const Sequelize = require("sequelize");
+const db = require("../db");
+// importing Product
+const Product = require("./models/Product");
 
-const OrderDetail = db.define('order_detail', {
+const OrderDetail = db.define("order_detail", {
   product_quantity: {
     type: Sequelize.INTEGER,
-  }
-})
+  },
+});
+
+OrderDetail.prototype.productTotal = async function () {
+  const orderItem = await Product.findByPk(this.product_id);
+  // productTotal is a function of (this.product_quantity X Product.price)
+  const prodTotal = orderItem.price * this.product_quantity;
+  return prodTotal;
+};
 
 module.exports = OrderDetails;
