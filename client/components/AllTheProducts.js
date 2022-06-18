163cd40d7d0f70f24675ed385301f08826f061f2
lkowal25 <leland.kowal@gmail.com>
2022-06-18 12:14:59 -0400

    added an ascending and descending sort

--- client/components/AllTheProducts.js
+++ client/components/AllTheProducts.js
@@ -1,14 +1,23 @@
 import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
+
 import { ProductContext } from './AllProducts';
+import ProductOrdering from './ProductOrdering';
 
 export default function AllTheProducts(props) {
   const { products } = useContext(ProductContext);
+  const [sortedProducts, setSortedProducts] = useState([]);
+  console.log('SORTED PROD ATP L 10', sortedProducts);
+  const finalProds = sortedProducts.length > 0 ? sortedProducts : products;
 
   return (
     <div>
+      <ProductOrdering
+        setSortedProducts={setSortedProducts}
+        products={products}
+      />
       <ul>
-        {products.map((product) => (
+        {finalProds.map((product) => (
           <li key={product.id}>
             {product.name}
             <Link to={`/products/${product.id}`}>
