ec6fa992dbfb6ec262d9e207919d5e032460b41c
lkowal25 <101602698+lkowal25@users.noreply.github.com>
2022-06-23 22:23:40 -0400

    Delete Stackathon directory

--- Stackathon/client/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import Audio from "./Audio";
-
-const root = createRoot(document.getElementById("app"));
-
-root.render(<Audio />);
